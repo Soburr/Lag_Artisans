5d3f756ce1d6e21204dfce0d4084fcee139b89a2
Soburr <adebesinbenzenejr99@gmail.com>
2024-10-15 10:00:58 +0100

    feature/landing-page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Lag Artisans</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -13,10 +13,6 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        <script src="//unpkg.com/alpinejs" defer></script>
-        <script src="https://cdn.tailwindcss.com"></script>
-        
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
