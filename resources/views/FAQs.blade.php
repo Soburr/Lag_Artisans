927a788869e6d8746b533d6de9c6e3596c9f24bf
Soburr <adebesinbenzenejr99@gmail.com>
2024-10-25 06:29:46 +0100

    feature/FAQs

--- resources/views/FAQs.blade.php
+++ resources/views/FAQs.blade.php
@@ -35,6 +35,41 @@
                <summary class="text-lg font-bold">How long does it take to complete a service?</summary>
                <p class="text-lg">Completion time depends on the mutual agreement of the clients and student-artisans.</p>
            </details>
+
+           <details>
+               <summary class="text-lg font-bold">How do I pay for services?</summary>
+               <p class="text-lg">The payment is based on agreement of the Clients and Student-artisans.</p>
+           </details>
+
+        </div>
+           <h2 class="text-2xl text-green-600 font-bold mb-2">Student-artisans</h2>
+
+           <div class="bg-gray-100 p-4 mb-4 rounded">
+            <details>
+                <summary class="text-lg font-bold">How do I become a student-artisan?</summary>
+                <p class="text-lg">Register as a student-artisan and clearly state what service offer .</p>
+            </details>
+
+            <details>
+                <summary class="text-lg font-bold">What are the benefits of being a student-artisan?</summary>
+                <p class="text-lg">Earn money, gain experience, and build your portfolio.</p>
+            </details>
+           </div>
+
+           <h2 class="text-2xl text-green-600 font-bold mb-2">Safety and Support</h2>
+
+           <div class="bg-gray-100 p-4 mb-4 rounded">
+            <details>
+                <summary class="text-lg font-bold">Is my personal information secure?</summary>
+                <p class="text-lg">Yes, we prioritize data protection.</p>
+            </details>
+
+            <details>
+                <summary class="text-lg font-bold">What if I have issues with a service/student-artisan?</summary>
+                <p class="text-lg">Contact our support team.</p>
+            </details>
+
+           </div>
         </div>
      </div>
 
